0eb8f5363caaa8649362043db38a60f31a1a1691
Hilal Akdoğan <107719630+akdoganhilal@users.noreply.github.com>
2023-08-01 23:47:58 +0300

    C# Genel
    C#

---
+++ h_renkliekolayzer/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace h_renkliekolayzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel9.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel8.Location = new System.Drawing.Point(229, 3);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(22, 240);
+            this.panel8.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.LightSalmon;
+            this.panel7.Location = new System.Drawing.Point(198, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(22, 240);
+            this.panel7.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.LightPink;
+            this.panel6.Location = new System.Drawing.Point(167, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(22, 240);
+            this.panel6.TabIndex = 7;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Crimson;
+            this.panel5.Location = new System.Drawing.Point(136, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(22, 240);
+            this.panel5.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.MediumOrchid;
+            this.panel4.Location = new System.Drawing.Point(105, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(22, 240);
+            this.panel4.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel2.Location = new System.Drawing.Point(74, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(22, 240);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(22, 240);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel3.Location = new System.Drawing.Point(43, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(22, 240);
+            this.panel3.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(283, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 41);
+            this.button1.TabIndex = 12;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel9.Controls.Add(this.panel1);
+            this.panel9.Controls.Add(this.panel3);
+            this.panel9.Controls.Add(this.panel2);
+            this.panel9.Controls.Add(this.panel4);
+            this.panel9.Controls.Add(this.panel5);
+            this.panel9.Controls.Add(this.panel6);
+            this.panel9.Controls.Add(this.panel8);
+            this.panel9.Controls.Add(this.panel7);
+            this.panel9.Controls.Add(this.textBox1);
+            this.panel9.Location = new System.Drawing.Point(12, 42);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(265, 249);
+            this.panel9.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(49, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 30);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "EKOLAYZER";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(283, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 41);
+            this.button2.TabIndex = 15;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(283, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 41);
+            this.button3.TabIndex = 16;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(283, 42);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 41);
+            this.button4.TabIndex = 17;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe Script", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox1.Location = new System.Drawing.Point(60, 225);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 18);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Text = "H i l a l  A k d o ğ a n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 299);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel9);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "h_renkliekolayzer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ h_renkliekolayzer/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace h_renkliekolayzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Height = random.Next(240);
+            panel2.Height = random.Next(240);
+            panel3.Height = random.Next(240);
+            panel4.Height = random.Next(240);
+            panel5.Height = random.Next(240);
+            panel6.Height = random.Next(240);
+            panel7.Height = random.Next(240);
+            panel8.Height = random.Next(240);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            this.BackColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        int x, y, z;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            timer3.Enabled = true;
+            x = random.Next(255);
+            y = random.Next(255);
+            z = random.Next(255);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            
+            panel1.BackColor = Color.FromArgb(x, y, z);
+            panel2.BackColor = Color.FromArgb(x, y, z);
+            panel3.BackColor = Color.FromArgb(x, y, z);
+            panel4.BackColor = Color.FromArgb(x, y, z);
+            panel5.BackColor = Color.FromArgb(x, y, z);
+            panel6.BackColor = Color.FromArgb(x, y, z);
+            panel7.BackColor = Color.FromArgb(x, y, z);
+            panel8.BackColor = Color.FromArgb(x, y, z);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            panel1.BackColor = Color.PaleGreen;
+            panel2.BackColor = Color.LightSteelBlue;
+            panel3.BackColor = Color.MediumTurquoise;
+            panel4.BackColor = Color.MediumOrchid;
+            panel5.BackColor = Color.Crimson;
+            panel6.BackColor = Color.LightPink;
+            panel7.BackColor = Color.LightSalmon;
+            panel8.BackColor = Color.OrangeRed;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            timer2.Enabled = true;
+            x = random.Next(255);
+            y = random.Next(255);
+            z = random.Next(255);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {           
+            panel1.BackColor = Color.FromArgb(x, y, z);
+            panel2.BackColor = Color.FromArgb(x, z, y);
+            panel3.BackColor = Color.FromArgb(y, z, x);
+            panel4.BackColor = Color.FromArgb(y, y, z);
+            panel5.BackColor = Color.FromArgb(z, x, y);
+            panel6.BackColor = Color.FromArgb(z, y, x);
+            panel7.BackColor = Color.FromArgb(x, x, z);
+            panel8.BackColor = Color.FromArgb(y, x, z);
+        }
+    }
+}
